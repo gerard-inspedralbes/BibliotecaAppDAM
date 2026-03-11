cd5bdb91922e57f8b04c535a1b59472547a41760
gtorrents <gtorrents@inspedralbes.cat>
2026-03-11 12:18:10 +0100

    Modificada classe Reserva

--- src/model/Reserva.java
+++ src/model/Reserva.java
@@ -1,4 +1,28 @@
 package model;
 
 public class Reserva {
+    private Usuari user;
+    private Llibre llibre;
+
+    public Reserva(Usuari user, Llibre llibre) {
+        this.user = user;
+        this.llibre = llibre;
+    }
+
+    public Usuari getUser() {
+        return user;
+    }
+
+    public Llibre getLlibre() {
+        return llibre;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva{" +
+                "user=" + user +
+                ", llibre=" + llibre +
+                '}';
+    }
 }
+
